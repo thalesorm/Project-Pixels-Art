8f3970b74ecd3259903aced7e295f940b9837152
Thales Santos <85639311+thalesorm@users.noreply.github.com>
2022-12-17 23:05:42 -0300

    :sparkles: feat: Adicionando paleta de cores

---
+++ script.js
@@ -0,0 +1,10 @@
+const coresPalet = document.getElementsByClassName('color');
+const colors = ['green', 'red', 'blue', 'yellow'];
+
+function paletColor() {
+  for (let i = 0; i < coresPalet.length; i += 1) {
+    coresPalet[i].style.backgroundColor = colors[i];
+    coresPalet[i].style.border = '1px solid black';
+  }
+}
+paletColor();
