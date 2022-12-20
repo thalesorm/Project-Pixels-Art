57e6008f6e2509ff50b0bd621b9e6409f68c0b09
Thales Santos <85639311+thalesorm@users.noreply.github.com>
2022-12-20 16:51:57 -0300

    :recycle: refactor: requisitos anteriores agora em uma função

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const coresPalet = document.getElementsByClassName('color');
-const colors = ['black', 'red', 'blue', 'yellow'];
-
-function paletColor() {
-  for (let i = 0; i < coresPalet.length; i += 1) {
-    coresPalet[i].style.backgroundColor = colors[i];
-    coresPalet[i].style.border = '1px solid black';
+function creatPalet() {
+  const paletColor = document.getElementById('color-palette');
+  const colors = ['black', 'red', 'blue', 'yellow'];
+  for (let i = 0; i < 4; i += 1) {
+    const quadrado = document.createElement('div');
+    quadrado.classList = 'color';
+    quadrado.style.backgroundColor = colors[i];
+    paletColor.appendChild(quadrado);
   }
 }
-paletColor();
+creatPalet();
